1bf9e5b3cae132619fe2719b401bbc6c66be85c4
pplan <zgjszhuhui@163.com>
2021-08-05 14:58:49 +0800

    third day

--- activiti-02/src/test/java/pri/hzhu/activiti/DeleteDefinitionTest.java
+++ activiti-02/src/test/java/pri/hzhu/activiti/DeleteDefinitionTest.java
@@ -3,6 +3,8 @@ package pri.hzhu.activiti;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.repository.ProcessDefinitionQuery;
 import org.junit.Test;
@@ -31,6 +33,37 @@ public class DeleteDefinitionTest {
             // 开启级联删除
             repositoryService.deleteDeployment(processDefinition.getDeploymentId(), true);
         }
+    }
+
+    @Test
+    public void deploy() {
+        // 1. 创建processEngine
+        ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+
+        // 2. 获取repositoryService
+        RepositoryService repositoryService = engine.getRepositoryService();
+
+        // 3. 使用service进行流程部署
+        Deployment deploy = repositoryService.createDeployment().name("测试监听")
+                .addClasspathResource("bpmn/testListener.bpmn")
+                .deploy();
+
+        System.out.println(String.format("流程部署ID: %s", deploy.getId()));
+        System.out.println(String.format("流程部署名字: %s", deploy.getName()));
+
+
+        // 4. 启动流程
+        startProcess();
+    }
+
+    private void startProcess() {
+        ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+        // 启动
+        RuntimeService runtimeService = engine.getRuntimeService();
+
+        runtimeService.startProcessInstanceByKey(Constant.LISTENER_KEY, "10086");
+        System.out.println(engine.getName());
+
     }
 
 }
--- activiti-02/src/test/java/pri/hzhu/activiti/TaskListenerTest.java
+++ activiti-02/src/test/java/pri/hzhu/activiti/TaskListenerTest.java
@@ -1,53 +1,20 @@
 package pri.hzhu.activiti;
 
-import org.activiti.engine.*;
-import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.repository.ProcessDefinitionQuery;
 import org.activiti.engine.task.Task;
-import org.junit.After;
 import org.junit.Test;
 
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 public class TaskListenerTest {
 
     private static final String TASK_KEY = "myProcess_1";
 
-    Map<String, Object> assignMap;
-
-    @Test
-    public void deploy() {
-        // 1. 创建processEngine
-        ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
-
-        // 2. 获取repositoryService
-        RepositoryService repositoryService = engine.getRepositoryService();
-
-        // 3. 使用service进行流程部署
-        Deployment deploy = repositoryService.createDeployment().name("测试监听")
-                .addClasspathResource("bpmn/testListener.bpmn")
-                .deploy();
-
-        System.out.println(String.format("流程部署ID: %s", deploy.getId()));
-        System.out.println(String.format("流程部署名字: %s", deploy.getName()));
-
-
-        // 4. 启动流程
-        startProcess();
-    }
-
-    private void startProcess() {
-        ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
-        // 启动
-        RuntimeService runtimeService = engine.getRuntimeService();
-
-        runtimeService.startProcessInstanceByKey(TASK_KEY);
-        System.out.println(engine.getName());
-
-    }
 
     @Test
     public void completeTask() {
